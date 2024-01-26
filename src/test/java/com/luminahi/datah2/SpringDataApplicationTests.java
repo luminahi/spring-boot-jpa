41f6db6b4b85dd060623ad580845ced197290b71
alex lima <delimaferreiraalex@gmail.com>
2024-01-26 14:15:51 -0300

    initial commit

---
+++ src/test/java/com/luminahi/datah2/SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luminahi.datah2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
